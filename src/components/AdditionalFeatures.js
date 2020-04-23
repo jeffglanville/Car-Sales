4424618cafd953b96f9d32ef79bbcd407fe6884e
Jeff Glanville <jeff.glanville@outlook.com>
2020-04-23 18:45:14 -0500

    update styling

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -4,7 +4,7 @@ import AdditionalFeature from './AdditionalFeature';
 const AdditionalFeatures = props => {
   return (
     <div className="content">
-      <h4>Additional Features</h4>
+      <h4 className="add-features">Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
